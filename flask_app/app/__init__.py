1ef1d3088cc3e830ba9d4d3ce9fc04de6202a0bb
Galdarkk <40453128+Galdarkk@users.noreply.github.com>
2018-06-21 10:35:32 +0300

    task2_tags

---
+++ flask_app/app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from flask_app.app.settings import MONGO_URI
+import subprocess
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = MONGO_URI
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def index():
+    users = mongo.db.user.find().sort('rating', -1)
+    return render_template('index.html', users=users)
+
+
+@app.route('/<user_name>')
+def user_profile(user_name):
+    user = mongo.db.user.find_one_or_404({'user_name': user_name})
+    return render_template('user.html', user=user)
+
+
+@app.route('/refresh', methods=['POST'])
+def refresh():
+    subprocess.call(['scrapy', 'runspider', 'app/spider/forum.py'])
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask_app/run.py
@@ -0,0 +1,6 @@
+from waitress import serve
+from flask_app.app import app
+import os
+
+if __name__ == '__main__':
+    serve(app, host=os.environ.get("HOST", "localhost"), port=os.environ.get("PORT", 3000), expose_tracebacks=True)
